ae596ccbae405f89f4eadb622ae098cb0497d2cf
Luciano <91077377+Luciano-C@users.noreply.github.com>
2022-04-21 10:24:07 -0400

    Cardgroup se utiliza para planetas también

--- src/js/component/cardgroup.js
+++ src/js/component/cardgroup.js
@@ -4,16 +4,18 @@ import "../../styles/cardGroup.css"
 
 import { Context } from "../store/appContext";
 
-const CardGroup = () => {
-    const { store, actions } = useContext(Context);
-    const characters = store.characters;
-
+const CardGroup = (props) => {
+   /*  const { store, actions } = useContext(Context); */
+    const arrayToMap = props.arrayToMap;
+    const mapParameters = props.parameters;
+    
     return (
         <div className="d-flex flex-column text-start fst-italic fw-bold">
-            <h2>Characters</h2>
+            <h2>{props.title}</h2>
             <div className="d-flex justify-content-center">
                 <ul className="cardGroupContainer">
-                    {characters.map((x, i) => <Card key={i} char_1="Status" char_2="Species" char_3="Gender" name={x.name} value_1={x.status} value_2={x.species} value_3={x.gender} src={x.image} />)}
+                    {arrayToMap.map((x, i) => <Card key={i} char_1="Status" char_2="Species" char_3="Gender" name={x.name} value_1={x[mapParameters.mp1]} value_2={x[mapParameters.mp2]} value_3={x[mapParameters.mp3]} src={x[mapParameters.mp0]} />)}
+                    
                 </ul>
             </div>
         </div>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -6,16 +6,17 @@ import { Context } from "../store/appContext";
 
 import "../../styles/home.css";
 import CardGroup from "../component/cardgroup";
-import PlanetGroup from "../component/planetgroup";
+
 
 
 export const Home = () => {
-	/* const { store, actions } = useContext(Context); */
+	
+	const { store, actions } = useContext(Context);
 
 	return (
 		<div>
-            <CardGroup/>
-			<PlanetGroup/>
+            <CardGroup title="Characters" arrayToMap={store.characters} parameters={{mp0: "image", mp1: "status", mp2: "species", mp3: "gender"}}/>
+			<CardGroup title="Planets" arrayToMap={store.planets} parameters={{mp0: "", mp1: "type", mp2: "dimension", mp3: ""}}/>
 		</div>
 	);
 };
