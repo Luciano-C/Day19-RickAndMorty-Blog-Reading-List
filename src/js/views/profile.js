eadd092df9873633007ed6560c368bcb17eb5f0d
Luciano <91077377+Luciano-C@users.noreply.github.com>
2022-04-22 12:57:15 -0400

    Cambios menores

--- src/js/views/profile.js
+++ src/js/views/profile.js
@@ -19,15 +19,15 @@ const Profile = () => {
         <div className="d-flex justify-content-center">
             <div className="d-flex profile-container">
                 <img className="profile-photo" src={store.characters[arrayPosition]?.image} />
-                <ul>
-                    <li>Name: {store.characters[arrayPosition]?.name}.</li>
-                    <li>Status: {store.characters[arrayPosition]?.status}.</li>
-                    <li>Species: {store.characters[arrayPosition]?.species}.</li>
-                    <li>Type: {store.characters[arrayPosition]?.type}.</li>
-                    <li>Gender: {store.characters[arrayPosition]?.gender}.</li>
-                    <li>Origin: {store.characters[arrayPosition]?.origin.name}.</li>
-                    <li>Location: {store.characters[arrayPosition]?.location.name}.</li>
-                    <li>Number of episodes: {store.characters[arrayPosition]?.episode.length}.</li>
+                <ul className="profile-ul">
+                    <li className="profile-list-item">Name: {store.characters[arrayPosition]?.name}.</li>
+                    <li className="profile-list-item">Status: {store.characters[arrayPosition]?.status}.</li>
+                    <li className="profile-list-item">Species: {store.characters[arrayPosition]?.species}.</li>
+                    <li className="profile-list-item">Type: {store.characters[arrayPosition]?.type}.</li>
+                    <li className="profile-list-item">Gender: {store.characters[arrayPosition]?.gender}.</li>
+                    <li className="profile-list-item">Origin: {store.characters[arrayPosition]?.origin.name}.</li>
+                    <li className="profile-list-item">Location: {store.characters[arrayPosition]?.location.name}.</li>
+                    <li className="profile-list-item">Number of episodes: {store.characters[arrayPosition]?.episode.length}.</li>
                 </ul>
             </div>
         </div>
